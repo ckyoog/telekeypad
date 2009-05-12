9848e54cc9c79bb2c60f64cee93bc5ca193e7525
kouhy <ckyoog@gmail.com>
2009-05-12 12:04:29 +0800

    Change comment of interface in telekeypad.h

--- telekeypad.h
+++ telekeypad.h
@@ -1,18 +1,23 @@
 #ifndef _TELEKEYPAD_H_
 #define _TELEKEYPAD_H_
 
-/* button click event callback */
+/* Parameters explaination of two main functions below:
+ * w: the main window which telekeypad dialog belongs to, can be NULL.
+ * data: anything you want to pass to telekeypad dialog, but currently
+ * 	 is useless, can be NULL.
+ */
+/* call this if you want to call out to somebody */
 void telekeypad_callout(GtkWindow *w, gpointer data);
-
-/* call this when telephone call is incomming */
+/* call this when call is incomming */
 void telekeypad_callin(const gchar *telenumber, GtkWindow *w, gpointer data);
 
-/* helper for load icon which is on button */
-void telekeypad_set_icon_path(const char *path);
 
-/* routines for set connect state */
+/* routines used for telling connect state to telekeypad dialog */
 void telekeypad_set_connecting();
 void telekeypad_set_connected();
 void telekeypad_set_disconnect();
+
+/* helper for load icon which is on button */
+void telekeypad_set_icon_path(const char *path);
 
 #endif	/* _TELEKEYPAD_H_ */
